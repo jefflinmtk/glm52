1c620ff7c61f808889e6de8828275afefd1e728c
jefflinmtk <jefflinmtk@gmail.com>
2026-07-03 15:01:31 +0800

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,5 @@
-print(" sudo apt update && sudo apt install -y nfs-kernel-server")
+print("  sudo bash -c 'cat >> /etc/exports <<EOF
+  /srv/hf 10.248.13.23(ro,sync,no_subtree_check,no_root_squash) 10.248.13.24(ro,sync,no_subtree_check,no_root_squash)
+  10.248.13.25(ro,sync,no_subtree_check,no_root_squash) 10.248.13.123(ro,sync,no_subtree_check,no_root_squash)
+  10.248.13.22(ro,sync,no_subtree_check,no_root_squash) 10.248.13.122(ro,sync,no_subtree_check,no_root_squash)
+  EOF'")
