be4af0f6b4e8d82cd6e7853726ef6ea6bbecf0da
jefflinmtk <jefflinmtk@gmail.com>
2026-07-03 15:08:49 +0800

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,8 @@
-print("sudo bash -c 'cat >> /etc/exports <<EOF /srv/hf 10.248.13.23(ro,sync,no_subtree_check,no_root_squash) 10.248.13.24(ro,sync,no_subtree_check,no_root_squash) 10.248.13.25(ro,sync,no_subtree_check,no_root_squash) 10.248.13.123(ro,sync,no_subtree_check,no_root_squash) 10.248.13.22(ro,sync,no_subtree_check,no_root_squash) 10.248.13.122(ro,sync,no_subtree_check,no_root_squash)  EOF'")
+# -*- coding: utf-8 -*-
+# 直接使用三引號，保留所有換行與空白
+command = """sudo bash -c 'cat >> /etc/exports <<EOF
+/srv/hf 10.248.13.23(ro,sync,no_subtree_check,no_root_squash) 10.248.13.24(ro,sync,no_subtree_check,no_root_squash)
+10.248.13.25(ro,sync,no_subtree_check,no_root_squash) 10.248.13.123(ro,sync,no_subtree_check,no_root_squash)
+10.248.13.22(ro,sync,no_subtree_check,no_root_squash) 10.248.13.122(ro,sync,no_subtree_check,no_root_squash)
+EOF'"""
+print(command)
