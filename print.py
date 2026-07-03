9d06d4ee4434f58a7be76690838979a9752f54ae
jefflinmtk <jefflinmtk@gmail.com>
2026-07-03 15:15:41 +0800

    Update print.py

--- print.py
+++ print.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
-# 直接使用三引號，保留所有換行與空白
-command = """sudo bash -c 'cat >> /etc/exports <<EOF
-/srv/hf 10.248.13.23(ro,sync,no_subtree_check,no_root_squash) 10.248.13.24(ro,sync,no_subtree_check,no_root_squash)
-10.248.13.25(ro,sync,no_subtree_check,no_root_squash) 10.248.13.123(ro,sync,no_subtree_check,no_root_squash)
-10.248.13.22(ro,sync,no_subtree_check,no_root_squash) 10.248.13.122(ro,sync,no_subtree_check,no_root_squash)
+
+# 用三引號包住整段文字，Python 會保留裡面的換行與空格
+cmd = """sudo bash -c 'cat > /etc/exports <<EOF
+/srv/hf 10.248.13.22(ro,sync,no_subtree_check,no_root_squash)
+/srv/hf 10.248.13.23(ro,sync,no_subtree_check,no_root_squash)
+/srv/hf 10.248.13.24(ro,sync,no_subtree_check,no_root_squash)
+/srv/hf 10.248.13.25(ro,sync,no_subtree_check,no_root_squash)
+/srv/hf 10.248.13.122(ro,sync,no_subtree_check,no_root_squash)
+/srv/hf 10.248.13.123(ro,sync,no_subtree_check,no_root_squash)
 EOF'"""
-print(command)
+
+print(cmd)
