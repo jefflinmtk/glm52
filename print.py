89f1e696c1ea11d0e6b7cf039c32e96313a15ef7
jefflinmtk <jefflinmtk@gmail.com>
2026-07-03 18:29:18 +0800

    Update print.py

--- print.py
+++ print.py
@@ -1,6 +1,6 @@
 
 cmd = """
-sudo docker exec -it $(sudo docker ps --filter name=node- --format '{{.Names}}' | head -1) ray status
+sudo docker rm -f $(sudo docker ps -aq --filter name=node-) 2>/dev/null; echo cleaned
 """
 
 print(cmd)
