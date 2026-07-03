0f477848d5908aabd9ce13871ce6bc5aca04fb1b
jefflinmtk <jefflinmtk@gmail.com>
2026-07-03 15:05:43 +0800

    Update print.py

--- print.py
+++ print.py
@@ -1,4 +1,1 @@
-print("sudo bash -c 'cat >> /etc/exports <<EOF /srv/hf 10.248.13.23(ro,sync,no_subtree_check,no_root_squash) 10.248.13.24(ro,sync,no_subtree_check,no_root_squash)
-  10.248.13.25(ro,sync,no_subtree_check,no_root_squash) 10.248.13.123(ro,sync,no_subtree_check,no_root_squash)
-  10.248.13.22(ro,sync,no_subtree_check,no_root_squash) 10.248.13.122(ro,sync,no_subtree_check,no_root_squash)
-  EOF'")
+print("sudo bash -c 'cat >> /etc/exports <<EOF /srv/hf 10.248.13.23(ro,sync,no_subtree_check,no_root_squash) 10.248.13.24(ro,sync,no_subtree_check,no_root_squash) 10.248.13.25(ro,sync,no_subtree_check,no_root_squash) 10.248.13.123(ro,sync,no_subtree_check,no_root_squash) 10.248.13.22(ro,sync,no_subtree_check,no_root_squash) 10.248.13.122(ro,sync,no_subtree_check,no_root_squash)  EOF'")
